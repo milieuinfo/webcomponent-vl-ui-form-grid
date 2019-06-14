8f82ed181b57dca22b411b6739ae1cf8bb840a73
driesva <Dries.VanAutreve@telenet.be>
2019-06-14 08:04:48 +0200

    ERK-1080 vl-ui-grid web component maken - demo aanpassen

--- vl-form-grid.src.js
+++ vl-form-grid.src.js
@@ -9,6 +9,17 @@ import {VlGrid, VlColumn} from "/node_modules/vl-ui-grid/vl-grid.js";
 export class VlFormGrid extends VlGrid {
   connectedCallback() {
     this.classList.add('vl-form-grid');
+    this._applyFormLabels();
+  }
+
+  get _labels() {
+    return this.querySelectorAll("label");
+  }
+
+  _applyFormLabels() {
+    this._labels.forEach(label => {
+      label.classList.add("vl-form__label");
+    });
   }
 }
 
@@ -19,8 +30,18 @@ export class VlFormGrid extends VlGrid {
  * @extends VlColumn
  */
 export class VlFormColumn extends VlColumn {
+
   connectedCallback() {
     this.classList.add('vl-form-column');
+    super.connectedCallback();
+  }
+
+  get _classPrefix() {
+    return 'vl-form-col--';
+  }
+
+  get _classPrefix() {
+    return 'vl-col--';
   }
 }
 
