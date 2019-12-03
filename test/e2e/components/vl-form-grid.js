25c608fb33ffdaae9eb20b925a622c90f796f494
cambiph <incubusattax@gmail.com>
2019-12-03 16:14:43 +0100

    Update gitignore

---
+++ test/e2e/components/vl-form-grid.js
@@ -0,0 +1,11 @@
+const { VlElement } = require('vl-ui-core');
+
+class VlFormGrid extends VlElement {  
+    constructor(driver, selector) {
+        super(driver, selector);
+    }
+
+    
+}
+
+module.exports = VlFormGrid;
