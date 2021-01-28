f2fccc020426d410794b70925de055b162f413c0
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 13:46:11 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-module.exports = {
-  VlFormGrid: require('../dist/vl-form-grid.src.js').VlFormGrid,
-  VlFormColumn: require('../dist/vl-form-grid.src.js').VlFormColumn,
-};
+export * from '../dist/vl-form-grid.src.js';
